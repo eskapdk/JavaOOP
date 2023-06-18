ff5feab9eb24425828ba0a342a16b047bc420e01
Eska <eskapdk@gmail.com>
2023-06-18 11:41:57 +0700

    tembahan inheritance

---
+++ src/ManagerApps.java
@@ -0,0 +1,13 @@
+public class ManagerApps {
+
+    public static void main(String[] args) {
+        var manager = new Manager();
+        manager.name = "Eska";
+        manager.sayHello("Ratih");
+
+        var vp = new VicePresident();
+        vp.name = "Eska";
+        vp.sayHello("Bayu");
+
+    }
+}
