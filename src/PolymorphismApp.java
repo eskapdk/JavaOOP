d51e5f2be36e1a4fdf4f5d08122468e32d663706
Eska Pratama Dian Kurniawan <eskapdk@gmail.com>
2023-06-23 15:47:41 +0700

    add check and cast

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -10,13 +10,21 @@ public class PolymorphismApp {
         employee = new VicePresident("Eska");
         employee.sayHello("ratih");
 
-
         sayHello(new Employee("Eska"));
         sayHello(new Manager("Ratih"));
         sayHello(new VicePresident("Bayu"));
     }
 
-    static void sayHello(Employee employee){
-        System.out.println("Halo "+employee.name);
+    static void sayHello(Employee employee) {
+        if (employee instanceof VicePresident) { // check tipe data
+            VicePresident vicePresident = (VicePresident) employee; // cast/konversi tipe data
+            System.out.println("Halo VP " + vicePresident.name);
+        } else if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Halo Manager " + manager.name);
+        } else {
+            System.out.println("Halo " + employee.name);
+        }
+
     }
 }
