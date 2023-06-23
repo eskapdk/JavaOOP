50d0c9727ce1368a4a04aa1bcdabaadf5681eb84
Eska Pratama Dian Kurniawan <eskapdk@gmail.com>
2023-06-23 17:59:17 +0700

    add abstract method

---
+++ src/Programmer/zaman/now/app/AnimalApp.java
@@ -0,0 +1,12 @@
+package Programmer.zaman.now.app;
+
+import Programmer.zaman.now.data.*;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        Animal animal = new Cat();
+        animal.name = "Cing Clik";
+        animal.run();
+
+    }
+}
