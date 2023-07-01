e09b8f70c06ce5f896096f43ff1414c080c558cc
Eska Pratama Dian Kurniawan <eskapdk@gmail.com>
2023-07-01 10:26:56 +0700

    add final class

---
+++ src/Programmer/zaman/now/data/SocialMedia.java
@@ -0,0 +1,11 @@
+package Programmer.zaman.now.data;
+
+class SocialMedia {
+String name;    
+}
+
+final class Facebook extends SocialMedia{
+
+}
+
+// class FakeFacebook extends Facebook{} EROOR karena sudah final class nya
