74423765ea8a2b271c055268f60fec398b7ca5db
Eska Pratama Dian Kurniawan <eskapdk@gmail.com>
2023-07-01 10:34:35 +0700

    add final method

--- src/Programmer/zaman/now/data/SocialMedia.java
+++ src/Programmer/zaman/now/data/SocialMedia.java
@@ -1,11 +1,17 @@
 package Programmer.zaman.now.data;
 
 class SocialMedia {
-String name;    
+    String name;
 }
 
-final class Facebook extends SocialMedia{
+class Facebook extends SocialMedia {
+    final void login(String username, String password) {
 
+    }
 }
 
-// class FakeFacebook extends Facebook{} EROOR karena sudah final class nya
+class FakeFacebook extends Facebook {
+    // void login(String username, String password) {
+
+    // } ERROR karena sudah final method
+}
