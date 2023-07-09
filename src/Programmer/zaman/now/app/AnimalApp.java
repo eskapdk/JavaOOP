15d6a4eacb0bca5883d28123206015d487431b29
Eska <eureka.elwing@gmail.com>
2023-07-09 12:54:48 +0700

    add annotation

--- src/Programmer/zaman/now/app/AnimalApp.java
+++ src/Programmer/zaman/now/app/AnimalApp.java
@@ -1,7 +1,9 @@
 package Programmer.zaman.now.app;
 
+import Programmer.zaman.now.annotation.Fancy;
 import Programmer.zaman.now.data.*;
 
+@Fancy(name = "Animal", tags = {"Application","Java"})
 public class AnimalApp {
     public static void main(String[] args) {
         Animal animal = new Cat();
